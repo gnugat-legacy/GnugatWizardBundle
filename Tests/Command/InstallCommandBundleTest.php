7da8395b5e411e6e07b392cf751b068be016689b
Loïc Chardonnet <loic.chardonnet@gmail.com>
2013-08-15 18:21:58 +0200

    improved test coverage

--- Tests/Command/InstallCommandBundleTest.php
+++ Tests/Command/InstallCommandBundleTest.php
@@ -2,6 +2,8 @@
 
 namespace SfFactory\BundleCommandBundle\Tests\Command;
 
+use SfFactory\BundleCommandBundle\Command\InstallBundleCommand;
+
 class InstallBundleCommandTest extends CommandTestCase
 {
     public function testDefaultCommand()
@@ -13,5 +15,29 @@ class InstallBundleCommandTest extends CommandTestCase
             'composer-package-name' => $packageName,
             'version' => '1.3.37',
         ));
+    }
+
+    public function testName()
+    {
+        $command = new InstallBundleCommand();
+
+        $this->assertSame('sf-factory:bundle:install', $command->getName());
+    }
+
+    public function testArguments()
+    {
+        $command = new InstallBundleCommand();
+        $definition = $command->getDefinition();
+
+        $this->assertTrue($definition->hasArgument('composer-package-name'));
+        $this->assertTrue($definition->hasArgument('version'));
+    }
+
+    public function testHelp()
+    {
+        $command = new InstallBundleCommand();
+
+        $expectedHelp = file_get_contents(__DIR__.'/../../Resources/synopsis.txt');
+        $this->assertSame($expectedHelp, $command->getHelp());
     }
 }
