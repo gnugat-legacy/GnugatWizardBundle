3b190da33b889b2ca20a99c51726831b0f887b6d
Loïc Chardonnet <loic.chardonnet@gmail.com>
2013-11-11 11:43:07 +0100

    [PostInstall] Updated installer script

--- bin/installer.sh
+++ bin/installer.sh
@@ -9,5 +9,24 @@ fi
 echo '[composer] Downloading the bundle'
 composer require "gnugat/wizard-bundle:~1.0"
 
+echo '[php] Subscribing to the post-package-install event'
+php -a <<EOF
+<?php
+\$composerConfigFile = file_get_contents('composer.json');
+\$composerConfig = json_decode(\$composerConfigFile, true);
+
+if (!isset(\$composerConfig['scripts'])) {
+    \$composerConfig['scripts'] = array();
+}
+
+if (!isset(\$composerConfig['scripts']['post-package-install'])) {
+    \$composerConfig['scripts']['post-package-install'] = array();
+}
+
+\$composerConfig['scripts']['post-package-install'][] = 'Gnugat\\Bundle\\WizardBundle\\EventListener\\ComposerEvent::postPackageInstall';
+\$composerConfigFile = json_encode(\$composerConfig, JSON_PRETTY_PRINT);
+file_put_contents('composer.json', \$composerConfigFile);
+EOF
+
 echo '[sed] Enabling the bundle'
 sed -i 's/        }/            $bundles[] = new Gnugat\\Bundle\\WizardBundle\\GnugatWizardBundle();\n        }/' app/AppKernel.php
